bc98e0b4a4952ba314f008751ac5d51e18751553
samuel-anderson <s07241990@gmail.com>
2023-07-01 23:54:22 -0700

    build navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import Home from "./routes/home/home.component";
-import { Routes, Route, Outlet } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navigation Bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
 };
-
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );

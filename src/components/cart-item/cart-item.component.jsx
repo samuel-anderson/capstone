fd3d606d3cac8e94235ca756bb48027a3a555624
samuel-anderson <s07241990@gmail.com>
2023-07-11 23:30:53 -0700

    fix cart item style

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,10 +1,15 @@
-import { CartItemContainer, ItemDetails, Name } from "./cart-item.styles";
+import {
+  CartItemContainer,
+  ItemDetails,
+  Name,
+  Image,
+} from "./cart-item.styles";
 const CartItem = ({ cartItem }) => {
   const { name, quantity, imageUrl, price } = cartItem;
 
   return (
     <CartItemContainer>
-      <img src={imageUrl} alt={`${name}`} />
+      <Image src={imageUrl} alt={`${name}`} />
       <ItemDetails>
         <Name>{name}</Name>
         <span className="price">

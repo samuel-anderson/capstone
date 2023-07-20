9749db2b1027671e955f749316287b0e80f395dc
samuel-anderson <s07241990@gmail.com>
2023-07-19 22:52:21 -0700

    migrate category context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,26 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setCategoriesMap } from "../../store/categories/categories.action";
+
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import "./shop.styles.scss";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    //wrap async operations inside of async function
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />

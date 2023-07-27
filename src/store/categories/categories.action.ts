f1e10a4d3f014dc02493cfba979bb674640480b6
samuel-anderson <s07241990@gmail.com>
2023-07-26 21:49:36 -0700

    typescript migration

---
+++ src/store/categories/categories.action.ts
@@ -0,0 +1,47 @@
+import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
+import {
+  createAction,
+  Action,
+  ActionWithPayload,
+} from "../../utils/reducer/reducer.utils";
+
+export type FetchCategoriesStart =
+  Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+export type FetchCategoriesSuccess = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+  Category[]
+>;
+export type FetchCategoriesFailure = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+  Error
+>;
+
+export type CategoryAction =
+  | FetchCategoriesStart
+  | FetchCategoriesSuccess
+  | FetchCategoriesFailure;
+
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (
+  categoriesArray: Category[]
+): FetchCategoriesSuccess =>
+  createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailure = (error: Error): FetchCategoriesFailure =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+//USED WITH THUNKS
+// export const fetchCategoriesAsync = () => async (dispatch) => {
+//   dispatch(fetchCategoriesStart());
+//   try {
+//     const categoriesArray = await getCategoriesAndDocuments("categories");
+//     dispatch(fetchCategoriesSuccess(categoriesArray));
+//   } catch (error) {
+//     dispatch(fetchCategoriesFailure(error));
+//   }
+// };
--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,6 +1,14 @@
-import { CATEGORIES_ACTION_TYPES } from "./categories.types";
+import { CATEGORIES_ACTION_TYPES, Category } from "./categories.types";
 
-const CATEGORIES_INITIAL_STATE = {
+import { CategoryAction } from "./categories.action";
+
+export type CategoriesState = {
+  readonly categories: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+const CATEGORIES_INITIAL_STATE: CategoriesState = {
   categories: [],
   isLoading: false,
   error: null,
@@ -8,27 +16,25 @@ const CATEGORIES_INITIAL_STATE = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {}
+  action = {} as CategoryAction
 ) => {
-  const { type, payload } = action;
-
   /*
   fundamental basis of the majority of our asynchronous actions when it comes to fetching inside of any
   redux based side effect library: redux-thunk, observable, and saga
 
 
   */
-  switch (type) {
+  switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return { ...state, isLoading: true };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
       return {
         ...state,
-        categories: payload,
+        categories: action.payload,
         isLoading: false,
       };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, isLoading: true, error: payload };
+      return { ...state, isLoading: true, error: action.payload };
     default:
       return state;
   }

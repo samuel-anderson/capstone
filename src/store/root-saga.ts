e9888a16b074dfafdd5092591fb4978f03fecc6f
samuel-anderson <s07241990@gmail.com>
2023-08-04 08:15:44 -0700

    revert changes

---
+++ src/store/root-saga.ts
@@ -0,0 +1,9 @@
+//Encapsulate all of the sagas
+
+import { all, call } from "typed-redux-saga/macro";
+import { categoriesSaga } from "./categories/categories.saga";
+import { userSagas } from "./user/user.saga";
+
+export function* rootSaga() {
+  yield* all([call(categoriesSaga), call(userSagas)]);
+}
